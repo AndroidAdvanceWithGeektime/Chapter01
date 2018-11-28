0f33eb9c5a269339afffeebd342d8df8050a5e24
sunpengfei <sunpengfei@didichuxing.com>
2018-11-28 17:48:22 +0800

    更改选择样式

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Chapter01-GEEKTIME
 项目构建
 =======
 
-项目采用 [CMAKE](https://developer.android.com/ndk/guides/cmake) 来构建 breakpad 和 ndcrash 库, 项目可直接导入 AndroidStudio 运行
+例子采用 [CMAKE](https://developer.android.com/ndk/guides/cmake) 来构建 breakpad 和 ndcrash 库, 项目可直接导入 AndroidStudio 运行
+
+NDCrash 是一款开源的 Native Crash 捕获框架，为了兼容各个系统版本实现了不同方式的 unwinder。
+
+例子支持`armeabi-v7a`,`arm64-v8a`,`x86` 三种平台，不过在某些手机上会某些 unwind 方式会失败。
 
 
+生成的 crash 都存放在 `/data/data/com.dodola.breakpad/files` 或者 `/sdcard/crashDump（给予权限）`下
--- app/src/main/java/com/dodola/breakpad/MainActivity.java
+++ app/src/main/java/com/dodola/breakpad/MainActivity.java
@@ -2,50 +2,103 @@ package com.dodola.breakpad;
 
 import android.Manifest;
 import android.app.Activity;
-import android.os.Build;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.RadioGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import ru.ivanarh.jndcrash.NDCrash;
 import ru.ivanarh.jndcrash.NDCrashError;
 import ru.ivanarh.jndcrash.NDCrashUnwinder;
 
 public class MainActivity extends Activity {
-
-  private static boolean test;
+  private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
 
   static {
     System.loadLibrary("dcrash");
   }
+
+  private String reportPath;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    final RadioGroup radioGroup = findViewById(R.id.radio_group);
     TextView tv = findViewById(R.id.sample_text);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(
+          this,
+          new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+          WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+    } else {
+      initReportPath();
     }
+
+    final Spinner spinner = findViewById(R.id.spinner);
+    ArrayAdapter<CharSequence> adapter =
+        ArrayAdapter.createFromResource(
+            this, R.array.crash_array, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+    spinner.setSelection(0);
+
     findViewById(R.id.crash_btn)
         .setOnClickListener(
             new View.OnClickListener() {
               @Override
               public void onClick(View view) {
-                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-                if (checkedRadioButtonId == R.id.radio_breakpad) {
-                  initBreakpad(getFilesDir().getAbsolutePath());
-                } else if (checkedRadioButtonId == R.id.radio_ndcrash) {
-                  final String reportPath =
-                      getFilesDir().getAbsolutePath() + "/crash.txt"; // Example.
+                int selectedItemPosition = spinner.getSelectedItemPosition();
+                String selectItem = (String) spinner.getSelectedItem();
+                if (selectedItemPosition == 0) {
+                  initBreakpad(reportPath);
+                  crash();
+                } else {
+
+                  NDCrashUnwinder unwinder = NDCrashUnwinder.libunwind;
+                  switch (selectedItemPosition) {
+                    case 1:
+                      unwinder = NDCrashUnwinder.libcorkscrew;
+                      break;
+                    case 2:
+                      unwinder = NDCrashUnwinder.libunwind;
+                      break;
+                    case 3:
+                      unwinder = NDCrashUnwinder.libunwindstack;
+                      break;
+                    case 4:
+                      unwinder = NDCrashUnwinder.cxxabi;
+                      break;
+                    case 5:
+                      unwinder = NDCrashUnwinder.stackscan;
+                      break;
+                    default:
+                      break;
+                  }
+                  if (TextUtils.isEmpty(reportPath)) {
+                    reportPath = getFilesDir().getAbsolutePath();
+                  }
                   final NDCrashError error =
                       NDCrash.initializeInProcess(
-                          reportPath, NDCrashUnwinder.libunwind); // 此处可以选择多个处理框架
+                          reportPath + "/crash_" + selectItem + ".txt", unwinder); // 此处可以选择多个处理框架
+                  if (error == NDCrashError.ok) {
+                    crash();
+                  } else {
+                    Toast.makeText(view.getContext(), "初始化失败 " + error, Toast.LENGTH_SHORT).show();
+                  }
                 }
-                crash();
               }
             });
   }
@@ -54,6 +107,16 @@ public class MainActivity extends Activity {
   public void onRequestPermissionsResult(
       int requestCode, String[] permissions, int[] grantResults) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    initReportPath();
+  }
+
+  private void initReportPath() {
+    File crashDumpDir = new File(Environment.getExternalStorageDirectory(), "crashDump");
+    if (!crashDumpDir.exists()) {
+      crashDumpDir.mkdirs();
+    }
+    reportPath = crashDumpDir.getAbsolutePath();
+    Log.d("xxxxx", "reportPath:" + reportPath);
   }
 
   public native void crash();
