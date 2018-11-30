5601e6030c58ac5bd5dd65ba1d20ff0c149c6637
sunpengfei <sunpengfei@didichuxing.com>
2018-11-30 23:11:26 +0800

    提交工具

--- sample/src/main/java/com/dodola/breakpad/MainActivity.java
+++ sample/src/main/java/com/dodola/breakpad/MainActivity.java
@@ -1,28 +1,80 @@
 package com.dodola.breakpad;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 
+import com.sample.breakpad.BreakpadInit;
+
+import java.io.File;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
 public class MainActivity extends Activity {
-  static {
-    System.loadLibrary("crash-lib");
-  }
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    static {
+        System.loadLibrary("crash-lib");
+    }
 
-    findViewById(R.id.crash_btn)
-        .setOnClickListener(
-            new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                crash();
-              }
-            });
-  }
+    private File externalReportPath;
 
-  public native void crash();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            initExternalReportPath();
+        }
+
+        findViewById(R.id.crash_btn)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                initBreakPad();
+                                crash();
+                                // copy core dump to sdcard
+                            }
+                        });
+    }
+
+    private void initBreakPad() {
+        if (externalReportPath == null) {
+            externalReportPath = new File(getFilesDir(), "crashDump");
+            if (!externalReportPath.exists()) {
+                externalReportPath.mkdirs();
+            }
+        }
+        BreakpadInit.initBreakpad(externalReportPath.getAbsolutePath());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        initExternalReportPath();
+    }
+
+    private void initExternalReportPath() {
+        externalReportPath = new File(Environment.getExternalStorageDirectory(), "crashDump");
+        if (!externalReportPath.exists()) {
+            externalReportPath.mkdirs();
+        }
+    }
+
+    public native void crash();
 }
